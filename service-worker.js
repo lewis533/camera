c299e8a24c542c3fbf0794960460cd85715ba717
lewis533 <lewiskimanik@gmail.com>
2026-03-05 15:13:18 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,35 @@
-const CACHE_NAME="myapp-cache-v1"
+const CACHE_NAME = "my-app-cache-v1";
 
-const FILES=[
+self.addEventListener("install", event=>{
+event.waitUntil(
+caches.open(CACHE_NAME).then(cache=>{
+return cache.addAll([
 "/",
 "/index.html",
 "/manifest.json"
-]
-
-self.addEventListener("install",event=>{
-event.waitUntil(
-caches.open(CACHE_NAME).then(cache=>{
-return cache.addAll(FILES)
+]);
 })
-)
-})
+);
+});
 
-self.addEventListener("fetch",event=>{
+self.addEventListener("fetch", event=>{
 event.respondWith(
 caches.match(event.request).then(response=>{
-return response || fetch(event.request)
+return response || fetch(event.request);
 })
-)
-})
+);
+});
 
-self.addEventListener("activate",event=>{
+self.addEventListener("activate", event=>{
 event.waitUntil(
 caches.keys().then(keys=>{
 return Promise.all(
 keys.map(key=>{
-if(key!==CACHE_NAME) return caches.delete(key)
+if(key !== CACHE_NAME){
+return caches.delete(key);
+}
 })
-)
-)
+);
 })
-)
-})
+);
+});
