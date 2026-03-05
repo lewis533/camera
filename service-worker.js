e6e043c4a7c58d1031a1eb87789917d20aeaf451
lewis533 <lewiskimanik@gmail.com>
2026-03-05 15:29:02 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,25 @@
-const CACHE_NAME = "my-app-cache-v1";
+const CACHE_NAME = "my-app-cache-v10";
 
-self.addEventListener("install", event=>{
-event.waitUntil(
-caches.open(CACHE_NAME).then(cache=>{
-return cache.addAll([
+const OFFLINE_URLS = [
 "/",
 "/index.html",
 "/manifest.json"
-]);
-})
-);
-});
+];
 
-self.addEventListener("fetch", event=>{
-event.respondWith(
-caches.match(event.request).then(response=>{
-return response || fetch(event.request);
-})
-);
-});
-
-self.addEventListener("activate", event=>{
+self.addEventListener("install", event => {
 event.waitUntil(
-caches.keys().then(keys=>{
+caches.open(CACHE_NAME).then(cache => {
+return cache.addAll(OFFLINE_URLS);
+})
+);
+self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+event.waitUntil(
+caches.keys().then(keys => {
 return Promise.all(
-keys.map(key=>{
+keys.map(key => {
 if(key !== CACHE_NAME){
 return caches.delete(key);
 }
@@ -32,4 +27,19 @@ return caches.delete(key);
 );
 })
 );
+self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+
+if(event.request.method !== "GET") return;
+
+event.respondWith(
+caches.match(event.request).then(response => {
+return response || fetch(event.request).catch(()=>{
+return caches.match("/index.html");
+});
+})
+);
+
 });
