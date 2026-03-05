7604c9e6fce3d173b262105ed43ec9910f183f05
lewis533 <lewiskimanik@gmail.com>
2026-03-05 18:44:52 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,25 @@
-const CACHE_NAME = "my-app-cache-v10";
+const CACHE_NAME = "myapp-cache-v1";
 
-const OFFLINE_URLS = [
-"/",
-"/index.html",
-"/manifest.json"
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json"
 ];
 
 self.addEventListener("install", event => {
-event.waitUntil(
-caches.open(CACHE_NAME).then(cache => {
-return cache.addAll(OFFLINE_URLS);
-})
-);
-self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-event.waitUntil(
-caches.keys().then(keys => {
-return Promise.all(
-keys.map(key => {
-if(key !== CACHE_NAME){
-return caches.delete(key);
-}
-})
-);
-})
-);
-self.clients.claim();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
 self.addEventListener("fetch", event => {
-
-if(event.request.method !== "GET") return;
-
-event.respondWith(
-caches.match(event.request).then(response => {
-return response || fetch(event.request).catch(()=>{
-return caches.match("/index.html");
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
-})
-);
